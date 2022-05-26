a2e8c835e0cc2ef83975af41614d4b3712eb1d5f
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-05-26 14:02:00 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,9 @@ function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // setTimeout(() => {
-    //   setLoading(false)
-    // }, 5000)
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000)
 
   }, [loading]);
 
