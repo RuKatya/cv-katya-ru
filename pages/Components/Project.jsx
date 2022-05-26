04ff6aac5a7f75f9a4f8ed4ba6f3428add547228
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-05-26 04:36:51 +0300

    Update Project.jsx

--- pages/Components/Project.jsx
+++ pages/Components/Project.jsx
@@ -3,19 +3,23 @@ import Image from "next/image";
 const Project = ({ project }) => {
   return (
     <>
-      <div key={project._id} className="projects__singleProject">
-        <Image
-          src={project.mainImg}
-          alt={project.title}
-          width={600}
-          height={600}
-          className="projects__singleProject--img"
-        />
-        <div className="projects__singleProject--title">
-          <h3>{project.title}</h3>
-          <button>Learn More</button>
+      {project ? (
+        <div key={project._id} className="projects__singleProject">
+          <Image
+            src={project.mainImg}
+            alt={project.title}
+            width={600}
+            height={600}
+            className="projects__singleProject--img"
+          />
+          <div className="projects__singleProject--title">
+            <h3>{project.title}</h3>
+            <button>Learn More</button>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div>Loading</div>
+      )}
     </>
   );
 };
