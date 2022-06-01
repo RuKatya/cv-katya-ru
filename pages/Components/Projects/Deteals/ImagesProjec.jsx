b9f8287a6d031ce0d5053e3ded44afcdf16e6dc8
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-06-01 13:14:57 +0300

    projects

--- pages/Components/Projects/Deteals/ImagesProjec.jsx
+++ pages/Components/Projects/Deteals/ImagesProjec.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const ImagesProjec = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return <div>ImagesProjec</div>;
 };
 
