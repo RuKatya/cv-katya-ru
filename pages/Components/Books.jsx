9ad77ee4586dcd69175b01f406beac3ce7231df2
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-06-11 16:58:14 +0300

    books

--- pages/Components/Books.jsx
+++ pages/Components/Books.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Books = () => {
   const booksArr = [
     { name: "Don't Make Me Think, 3th Edition", author: "Steve Krug" },
+    { name: "Rocket Surgery Made Easy", author: "Steve Krug" },
     { name: "Law of UX", author: "Jon Yablonski" },
     {
       name: "Computer Networks, 5th Edition",
